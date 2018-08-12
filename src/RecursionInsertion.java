31fbd93e4dffe96ea7e1a6da7548b61a822a1af9
Govardhan <redya01@ca.com>
2018-08-12 09:18:33 +0530

    First commit

---
+++ src/RecursionInsertion.java
@@ -0,0 +1,32 @@
+public class RecursionInsertion {
+
+    public static void main(String args[]){
+
+        int a[]={6,5,4,3,2,1};
+
+        insertion(a,a.length-1);
+
+        for(int x:a){
+            System.out.println(x);
+        }
+
+    }
+
+    private static void insertion(int a[],int index){
+
+        if(index>0){
+            insertion(a,index-1);
+            insert(a,index);
+        }
+    }
+
+    private static void insert (int a[],int index){
+        int key=a[index];
+        int i=index-1;
+        while(i>=0 && a[i]>key){
+            a[i+1]=a[i];
+            i--;
+        }
+        a[i+1]=key;
+    }
+}
